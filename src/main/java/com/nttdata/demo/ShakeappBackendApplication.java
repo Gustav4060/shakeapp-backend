e8a6d1b3635ed09283df28c1ee1a5efa33fc12f2
Gustav4060 <gustav4060eps@gmail.com>
2022-06-08 17:13:45 -0500

    configuraciones

---
+++ src/main/java/com/nttdata/demo/ShakeappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakeappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakeappBackendApplication.class, args);
+	}
+
+}
